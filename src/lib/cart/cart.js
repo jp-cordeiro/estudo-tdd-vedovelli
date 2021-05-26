7517c0a83f6f080c6b00d87f846804ee90dea768
JP <jpc.araujo@hotmail.com>
2021-05-25 23:28:03 -0300

    test - ensure no more than on product exists at a time

--- src/lib/cart/cart.js
+++ src/lib/cart/cart.js
@@ -1,6 +1,11 @@
+import { find, remove } from 'lodash';
 export default class Cart {
   items = [];
   addItem(item) {
+    const itemToFind = { product: item.product };
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind);
+    }
     this.items.push(item);
   }
   getTotal() {
--- src/lib/cart/cart.spec.js
+++ src/lib/cart/cart.spec.js
@@ -2,9 +2,14 @@ import Cart from './cart';
 
 describe('Cart', () => {
   let cart;
+  let product;
 
   beforeEach(() => {
     cart = new Cart();
+    product = {
+      title: 'Action Figure Luffy',
+      price: 35388,
+    };
   });
 
   it('return 0 when getTotal() is executed in a newly created instance', () => {
@@ -13,16 +18,29 @@ describe('Cart', () => {
 
   it('multiply quantity and price and receive the total amount ', () => {
     const item = {
-      product: {
-        title: 'Action Figure Luffy',
-        price: 35388,
-      },
+      product,
       quantity: 2,
     };
 
     const totalExpected = 2 * 35388;
 
     cart.addItem(item);
+    expect(cart.getTotal()).toEqual(totalExpected);
+  });
+
+  it('ensure no more than on product exists at a time', () => {
+    cart.addItem({
+      product,
+      quantity: 2,
+    });
+
+    cart.addItem({
+      product,
+      quantity: 1,
+    });
+
+    const totalExpected = 35388;
+
     expect(cart.getTotal()).toEqual(totalExpected);
   });
 });
